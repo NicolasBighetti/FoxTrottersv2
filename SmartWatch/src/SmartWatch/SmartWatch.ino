0f71e1937bc6cd63eb896e7bd06277a6ca427b7c
Benj <benja.piat@gmail.com>
2017-05-31 15:16:58 +0200

    PIAM-88 Notification showed on lcd screen

--- SmartWatch/src/SmartWatch/SmartWatch.ino
+++ SmartWatch/src/SmartWatch/SmartWatch.ino
@@ -6,10 +6,13 @@
 
 #include <Arduino.h>
 #include <CurieBLE.h>
+#include <LiquidCrystal.h>
 
 BLEPeripheral blePeripheral;  // BLE Peripheral Device (the board you're programming)
 BLEService comService("e582195d-50e4-4fc0-8dca-a8b10704694d"); // Communication Service
 BLECharacteristic message("e582195d-50e4-4fc0-8dca-a8b10704694d", BLERead | BLEWrite,16);
+LiquidCrystal lcd(10, 11, 12, 13, 14, 15, 16);
+
 
 char typedMessage[32];
 
@@ -17,7 +20,7 @@ void setup()
 {
 
   Serial.begin(9600);
-  while (!Serial);
+  //while (!Serial);
   Serial.println("Beginning");
   blePeripheral.setLocalName("FoxTrotters_SmartWatch");
   blePeripheral.setAdvertisedServiceUuid(comService.uuid());
@@ -36,6 +39,13 @@ void setup()
   Serial.println(comService.uuid());
   Serial.println(String(BLENotify));
 
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("My FoxTrotters");
+  lcd.setCursor(2,1); // set to the 3th column and 2nd row
+  lcd.print("watch");
+
   Serial.println("Ready to start BLE");
 
   blePeripheral.begin();
@@ -44,28 +54,22 @@ void setup()
 
 void loop()
 {
-  Serial.println("loop");
 
-  /*BLECentral central = blePeripheral.central();
+  BLECentral central = blePeripheral.central();
 
   if(central){
     Serial.println("Connected to central");
 
     while (central.connected()) {
       if(message.written()){
-        // turn the LED on (HIGH is the voltage level)
-        digitalWrite(LED_BUILTIN, HIGH);
-        // wait for a second
-        delay(1000);
-        // turn the LED off by making the voltage LOW
-        digitalWrite(LED_BUILTIN, LOW);
-         // wait for a second
+       
         sprintf(typedMessage,"%16c",NULL);
         strncpy(typedMessage,(char*)message.value(),message.valueLength());
-        Serial.println(typedMessage);
+        lcd.clear();
+        lcd.print(typedMessage);
       }
     }
-  }*/
+  }
 
   delay(1000);
 }
