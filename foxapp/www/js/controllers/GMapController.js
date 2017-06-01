fed1ccbc0a5947475768717a42ad12643bf71a3d
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-01 10:45:46 +0200

    [conflit] PIAM-35 renommage du controller
    conflit avec ngmap

---
+++ foxapp/www/js/controllers/GMapController.js
@@ -0,0 +1,14 @@
+angular.module('foxapp')
+
+.controller('GMapController', ['$scope', 'NgMap',
+
+  function() {
+    /*NgMap.getMap().then(function(map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+
+
+  });*/
+    console.log("arachide");
+}]);
