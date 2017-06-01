fe67ef4e5487814a00e1fdd2dd42ff8570fc2e5e
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-01 14:15:37 +0200

    [integration] PIAM-96 filtrage par nom désormais possible sur les POI

--- foxapp/www/js/services/MarkerService.js
+++ foxapp/www/js/services/MarkerService.js
@@ -9,21 +9,25 @@ angular.module('foxapp')
         getMarkers: function () {
           var mock = {
 
-            "coords":
+            "markers":
             [
               {
+                "nom": "thym",
                 "latitude": 43.7025,
                 "longitude": 7.26333
               },
               {
+                "nom": "oursins",
                 "latitude": 43.3025,
                 "longitude": 7.56333
               },
               {
+                "nom": "romarin",
                 "latitude": 43.9925,
                 "longitude": 7.16333
               },
               {
+                "nom": "thym",
                 "latitude": 43.7425,
                 "longitude": 7.29333
               }
