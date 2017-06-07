789c0adb176e5fda90ab6754e62c9d1f8f99e900
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-07 12:21:55 +0200

    [mock] PIAM-119 allignement des markeurs mock sur le modèle

--- foxapp/www/js/services/MarkerService.js
+++ foxapp/www/js/services/MarkerService.js
@@ -12,38 +12,42 @@ angular.module('foxapp')
         },
 
         getMarkers: function () {
-          var mock = {
-
-            "markers":
-            [
+          var mock =[
               {
-                "nom": "thym",
-                "theme":"plante",
-                "latitude": 43.7025,
-                "longitude": 7.26333
+                "name": "thym",
+                "typep":"plant",
+                "coords" : {
+                  "latitude": 43.7025,
+                  "longitude": 7.26333
+                }
               },
               {
-                "nom": "oursins",
-                "theme":"test",
-                "latitude": 43.3025,
-                "longitude": 7.56333
+                "name": "oursins",
+                "typep":"water",
+                "coords" : {
+                  "latitude": 43.3025,
+                  "longitude": 7.56333
+                }
               },
               {
-                "nom": "romarin",
-                "theme":"plante",
-                "latitude": 43.9925,
-                "longitude": 7.16333
+                "name": "romarin",
+                "typep":"plant",
+                "coords": {
+                  "latitude": 43.9925,
+                  "longitude": 7.16333
+                }
               },
-              {
-                "nom": "thym",
-                "theme":"plante",
+            {
+              "name": "thym",
+              "typep":"plant",
+              "coords": {
                 "latitude": 43.7425,
                 "longitude": 7.29333
               }
 
-            ]
+            }
+]
 
-        };
 
           return mock;
         }
