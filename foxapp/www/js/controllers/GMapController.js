b832efed6f3e923d5093a7dca68d4be11d3136d0
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-08 15:57:58 +0200

    [ajout] PIAM-122 PIAM-123 ajout gps et centrage de la carte

--- foxapp/www/js/controllers/GMapController.js
+++ foxapp/www/js/controllers/GMapController.js
@@ -1,19 +1,47 @@
 angular.module('foxapp')
 
-.controller('GMapController', ['$scope', '$window','$location', 'NgMap', 'MarkerService',
+.controller('GMapController', ['$scope', '$window','$location', '$cordovaGeolocation' ,'NgMap', 'MarkerService',
 
-  function($scope, $window ,$location, NgMap, MarkerService) {
+  function($scope, $window ,$location, $cordovaGeolocation, NgMap, MarkerService) {
     $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyAGMBQQC143VTbPWjLWEBJfB3LSzD0LnPw";
     //console.log(MarkerService.getMarkers());
+
+    $scope.foxMarkers = MarkerService.getMarkers();
 
     MarkerService.getAllMarkers().then(function(data) {
       $scope.foxMarkers = data;
     });
 
+    $scope.center = function() {
+
+      var posOptions = [];
+
+      $cordovaGeolocation.getCurrentPosition(posOptions)
+        .then(function (position) {
+          var coord = [];
+
+
+          coord.lat = position.coords.latitude;
+          coord.long = position.coords.longitude;
+          $scope.position = coord;
+
+          console.log('la localisation');
+
+        }, function (err) {
+          var coord = [];
+          console.log('default coord');
+          console.log(err);
+          coord.lat = 43.6156;
+          coord.long = 7.0719;
+          $scope.position = coord;
+        });
+    };
+
+    $scope.center();
 
     $scope.tinderise = function(){
       $scope.tinderswitch = false;
-    }
+    };
 
     $scope.swipeend = function(){
       $scope.tinderswitch = true;
