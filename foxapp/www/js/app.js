ffd4958e8815f6e5b35391b3a3bc383bfd839057
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-08 21:57:08 +0200

    [ajout] PIAM-134 ajout de la dépendance pour l'upload d'images

--- foxapp/www/js/app.js
+++ foxapp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('foxapp', ['ionic', 'ui.bootstrap.modal','ngMap', 'ngCordova', 'ngTouch', 'ngSwippy', 'ngRoute'])
+angular.module('foxapp', ['ionic', 'ui.bootstrap.modal','ngMap', 'ngFileUpload','ngCordova', 'ngTouch', 'ngSwippy', 'ngRoute'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
