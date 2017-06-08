909c34eb9f5d0155362a5287be2e33ce0cb25980
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-08 23:36:32 +0200

    [refactor] PIAM-134 passe à un service d'upload
    Et le yolo total de git sur le controller Photo

--- foxapp/www/js/controllers/PhotoController.js
+++ foxapp/www/js/controllers/PhotoController.js
@@ -1,41 +1,46 @@
 angular.module('foxapp')
 
-  .controller('PhotoController',['$scope', '$cordovaCamera', '$ionicPopup',
+.controller('PhotoController', ['$scope', 'PictureUploadService', '$cordovaCamera',
 
-    function($scope, $cordovaCamera, $ionicPopup) {
+  function($scope, PictureUploadService, $cordovaCamera) {
 
-      document.addEventListener("deviceready", function () {
+  document.addEventListener("deviceready", function () {
 
-        $scope.options = {
-          quality: 50,
-          destinationType: Camera.DestinationType.DATA_URL,
-          sourceType: Camera.PictureSourceType.CAMERA,
-          allowEdit: true,
-          encodingType: Camera.EncodingType.JPEG,
-          targetWidth: 100,
-          targetHeight: 100,
-          popoverOptions: CameraPopoverOptions,
-          saveToPhotoAlbum: false,
-          correctOrientation:true
-        };
-          }, false);
+    $scope.options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false,
+      correctOrientation:true
+    };
+  }, false);
 
+  $scope.URL = [{
+    "plantinder": "http://ns388671.ip-176-31-254.eu/api/planders",
+    "poi" : "http://ns388671.ip-176-31-254.eu/api/poi"
+  }];
 
-      $scope.takePicture = function(options) {
-        $cordovaCamera.getPicture(options).then(function (imageData) {
-          var image = document.getElementById('myImage');
-          image.src = /*"data:image/jpeg;base64," +*/ imageData;
-        }, function (err) {
-          // error
-        });
-      }
+  $scope.takePicture = function(options) {
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+      //var image = document.getElementById('myImage');
+      //:image.src = /*"data:image/jpeg;base64," +*/ imageData;
+      $scope.$file = imageData;
+    }, function (err) {
+      // error
+    });
+  };
 
-      $scope.sendPicture = function () {
+  $scope.sendPicture = function (URL, image) {
+    PictureUploadService.upload(image, URL);
+  };
 
-      }
+  $scope.close = function(){
+    $scope.uploadHide = true;
+  }
 
-      $scope.close = function(){
-        $scope.uploadHide = true;
-      }
-
-    }]);
+}]);
