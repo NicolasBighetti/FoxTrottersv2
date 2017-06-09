14b08f3ed72b397a6a8c3d8010d9018349c2c5ee
Flavian JACQUOT <flavian.jacquot@gmail.com>
2017-06-09 04:04:32 +0200

    ajoute l'upload de photo PIAM-134

--- foxapp/www/js/controllers/PhotoController.js
+++ foxapp/www/js/controllers/PhotoController.js
@@ -1,52 +1,54 @@
 angular.module('foxapp')
 
-.controller('PhotoController', ['$scope', 'PictureUploadService', '$cordovaCamera',
+  .controller('PhotoController', ['$scope', 'PictureUploadService', '$cordovaCamera',
 
-  function($scope, PictureUploadService, $cordovaCamera) {
+    function ($scope, PictureUploadService, $cordovaCamera) {
 
-  document.addEventListener("deviceready", function () {
+      document.addEventListener("deviceready", function () {
 
-    $scope.options = {
-      quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 100,
-      targetHeight: 100,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false,
-      correctOrientation:true
-    };
-  }, false);
+        $scope.options = {
+          quality: 50,
+          destinationType: Camera.DestinationType.FILE_URI,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 100,
+          targetHeight: 100,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false,
+          correctOrientation: true
+        };
+      }, false);
 
-  $scope.URL = {
-    "plantinder": "http://ns388671.ip-176-31-254.eu/api/planders",
-    "poi" : "http://ns388671.ip-176-31-254.eu/api/poi"
-  };
+      $scope.URL = {
+        "plantinder": "http://ns388671.ip-176-31-254.eu/api/planders",
+        "poi": "http://ns388671.ip-176-31-254.eu/api/poi"
+      };
 
-  $scope.takePicture = function(options) {
-    return $cordovaCamera.getPicture(options).then(function (imageData) {
-      //var image = document.getElementById('myImage');
-      //:image.src = /*"data:image/jpeg;base64," +*/ imageData;
-      return imageData;
-    }, function (err) {
-      // error
-    });
-  };
+      $scope.takePicture = function (options) {
+        return $cordovaCamera.getPicture(options).then(function (imageData) {
+          //var image = document.getElementById('myImage');
+          //:image.src = /*"data:image/jpeg;base64," +*/ imageData;
+          console.log(imageData);
 
-  $scope.sendPicture = function (URL, image) {
-    image.then( function(imageData){
+          return imageData;
+        }, function (err) {
+          // error
+        });
+      };
 
-      console.log(imageData);
-      console.log('chibre');
-        PictureUploadService.upload(imageData, URL);
+      $scope.sendPicture = function (URL, image) {
+        image.then(function (imageData) {
+
+            console.log(imageData);
+            console.log('hhhh');
+            PictureUploadService.upload(imageData, URL);
+          }
+        );
+      };
+
+      $scope.close = function () {
+        $scope.uploadHide = true;
       }
-    );
-  };
 
-  $scope.close = function(){
-    $scope.uploadHide = true;
-  }
-
-}]);
+    }]);
