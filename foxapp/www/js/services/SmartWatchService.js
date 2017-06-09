db9a45cf08406eadd6db5530aaa876aff3c4b3f1
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-09 03:09:19 +0200

    [montre] PIAM-107 tentative de notification 1

--- foxapp/www/js/services/SmartWatchService.js
+++ foxapp/www/js/services/SmartWatchService.js
@@ -13,14 +13,40 @@ angular.module('foxapp')
     //Le wrapper est défini dans RichNotificationWrapper
     //il implémente le js du plugin décrit /home/user/projuin/fox_trotters/private/foxapp/plugins/com.samsung.richnotification/www/richnotification.js
     //C'est le yolo total après, mais au moins ça passe la compilation angular, à prioris ça devrait marcher
-
+    //Samsung Gear > Notifications > Limit notifications
     function ($cordovaGear) {
       return {
         notificate : function(){
             //var jsonPOI = {name: namePOI, pos: positionPOI};
           if($cordovaGear.isSupported() && $cordovaGear.isConnected()){
-            $cordovaGear.send();
+            var opts = {
+              "uuid" : "",
+              "readoutTitle" : "Shaprrr",
+              "readout" : "Shaprrr",
+              "notificationTitle" : "Shaprrr",
+              "headerSizeType" : undefined,
+              "primarySubHeader" : "Shaprrrr",
+              "primaryBody" : "",
+              "primaryQRImage" : "",
+              "primaryBackgroundColor" : "#008000",
+              "primaryBackgroundImage" : "",
+              "secondaryType" : undefined,
+              "secondarySubHeader" : "",
+              "secondaryContent" : "",
+              "secondaryBackgroundColor" : "",
+              "secondaryImage" : "",
+              "smallIcon1Path" : "",
+              "smallIcon1Text" : "",
+              "smallIcon2Path" : "",
+              "smallIcon2Text" : "",
+              "notificationIcon" : "",
+              "alertType" : "104",
+              "popupType"  : "202",
+              "actions" : null
+          };
+              $cordovaGear.send(opts);
+          }
           }
         }
-      }
+
     }]);
