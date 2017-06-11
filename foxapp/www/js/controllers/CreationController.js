e8a4b9091fbaf33effa3a47ccbe08b747a158252
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-11 21:56:12 +0200

    [ajout] PIAM-114 upload de l'image associé à un POI a la creation

--- foxapp/www/js/controllers/CreationController.js
+++ foxapp/www/js/controllers/CreationController.js
@@ -3,30 +3,44 @@
  */
 angular.module('foxapp')
 
-.controller('CreationController', ['$scope','GeolocationService','RESTService',
+.controller('CreationController', ['$scope','GeolocationService','RESTService','PictureUploadService',
 
-  function($scope, GeolocationService, RESTService){
+  function($scope, GeolocationService, RESTService, PictureUploadService){
 
+    $scope.url = "http://ns388671.ip-176-31-254.eu/api/planders";
 
     $scope.createPOI = function(){
-      GeolocationService.getCurrentPosition({}).then(
-        function(data){
+
           $scope.poi.coords = {};
-          $scope.poi.coords.latitude = data.lat;
-          $scope.poi.coords.longitude = data.lng;
-        }
-      ).then(
-        function(){
+          $scope.poi.coords.latitude = $scope.position.lat;
+          $scope.poi.coords.longitude = $scope.position.lng;
+
           console.log($scope.poi);
-          RESTService.post('http://ns388671.ip-176-31-254.eu/api/pois', $scope.poi);
-        }
-      );
+
+
+
+      PictureUploadService.upload($scope.pictureURI,$scope.url).then(function(data){
+        var json = JSON.parse(data);
+        $scope.poi.image = json.image;
+        RESTService.post('http://ns388671.ip-176-31-254.eu/api/pois', $scope.poi);
+      });
+
       $scope.cancel();
     };
 
     $scope.form = {};
 
     $scope.poi = {};
+
+    $scope.pictureURI = "";
+
+    $scope.setURI = function(pictureData){
+      pictureData.then(function(path){
+        $scope.pictureURI = path;
+      });
+
+
+    }
 
     $scope.open = function() {
 
