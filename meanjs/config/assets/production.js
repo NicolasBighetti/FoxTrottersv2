7bb12d5b741243156dae92ec4fbfec860ad4aa82
Flavian JACQUOT <flavian.jacquot@gmail.com>
2017-06-12 16:39:56 +0200

    ajoute la vue des pois sur app web PIAM-149

--- meanjs/config/assets/production.js
+++ meanjs/config/assets/production.js
@@ -17,18 +17,22 @@ module.exports = {
         'public/lib/angular/angular.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/ng-file-upload/ng-file-upload.min.js',
         'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-mocks/angular-mocks.js',
         'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/lodash/lodash.js',
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/ng-file-upload/ng-file-upload.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/ngmap/build/scripts/ng-map.min.js',
         'public/lib/angular-simple-logger/dist/angular-simple-logger.min.js',
-        '//maps.google.com/maps/api/js?key=AIzaSyAGMBQQC143VTbPWjLWEBJfB3LSzD0LnPw', // I'm using a CDN
-        'public/lib/ngmap/build/scripts/ng-map.min.js'
+        'public/lib/lodash/lodash.js',
+        'public/lib/markerclustererplus/src/markerclusterer.min.js',
+        'public/lib/google-maps-utility-library-v3-markerwithlabel/dist/markerwithlabel.min.js',
+        'public/lib/google-maps-utility-library-v3-infobox/dist/infobox.min.js',
+        'public/lib/google-maps-utility-library-v3-keydragzoom/dist/keydragzoom.min.js',
+        'public/lib/js-rich-marker/src/richmarker.js',
+        'public/lib/angular-google-maps/dist/angular-google-maps.min.js',
         // endbower
       ]
     },
