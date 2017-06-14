291a3233512b1e5dd5ed48b321c851cd637e77d0
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-14 15:11:14 +0200

    [refactor] PIAM-163 ça commence à slider

--- foxapp/www/js/RouteConfiguration.js
+++ foxapp/www/js/RouteConfiguration.js
@@ -1,24 +1,18 @@
 /**
  * Created by user on 01/06/17.
  */
-angular.module('RouteConfiguration', ['ngRoute'])
-  .config(function($routeProvider, $locationProvider){
+angular.module('foxapp')
+  .config(function($stateProvider, $urlRouterProvider){
 
-    $routeProvider
-      .when('/',{
-          templateUrl: 'page/map.html',
-          controller: 'js/controllers/GMapController.js'
-        }
-      )
-      .when('foxapp/plantinder',{
-        templateUrl:'page/plantinder.html',
-        controller: 'js/controllers/PlantinderController.js'
-        }
+    $stateProvider
+      .state('map', {
+        url: '/',
+        templateUrl: 'view/map.html',
+        controller: 'GMapController'
+      });
 
-      )
-      .otherwise({
-      redirect: '/'
-    });
+    console.log('vrooom');
+    $urlRouterProvider.otherwise("/");
 
   });
 
--- foxapp/www/js/app.js
+++ foxapp/www/js/app.js
@@ -4,7 +4,7 @@ var dbPath = "http://ns388671.ip-176-31-254.eu/";
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('foxapp', ['ionic', 'ui.bootstrap.modal','ngMap', 'ngFileUpload','ngCordova', 'ngTouch', 'ngSwippy', 'ngRoute'])
+angular.module('foxapp', ['ionic', 'ui.bootstrap.modal','ngMap', 'ngFileUpload','ngCordova', 'ngTouch', 'ngSwippy'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
