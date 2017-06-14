bb24cef4958fa14129387d3bee71974911edeed0
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-15 01:10:30 +0200

    [refactor] PIAM-168 refactor du photoController

--- foxapp/www/js/controllers/PhotoController.js
+++ foxapp/www/js/controllers/PhotoController.js
@@ -1,44 +1,23 @@
 angular.module('foxapp')
 
-  .controller('PhotoController', ['$scope', 'PictureUploadService', '$cordovaCamera', 'GeolocationService', 'MarkerService',
+  .controller('PhotoController', ['$scope', 'PictureUploadService', 'GeolocationService', 'MarkerService','PhotoService',
 
-    function ($scope, PictureUploadService, $cordovaCamera, GeolocationService, MarkerService) {
+    function ($scope, PictureUploadService, $cordovaCamera, GeolocationService, MarkerService, PhotoService) {
 
-      document.addEventListener("deviceready", function () {
-
-        $scope.options = {
-          quality: 50,
-          destinationType: Camera.DestinationType.FILE_URI,
-          sourceType: Camera.PictureSourceType.CAMERA,
-          allowEdit: false,
-          encodingType: Camera.EncodingType.JPEG,
-          targetWidth: 100,
-          targetHeight: 100,
-          popoverOptions: CameraPopoverOptions,
-          saveToPhotoAlbum: false,
-          correctOrientation: true
-        };
-      }, false);
-
-
-
-      $scope.takePicture = function (options) {
-        return $cordovaCamera.getPicture(options).then(function (imageData) {
-          //var image = document.getElementById('myImage');
-          //:image.src = /*"data:image/jpeg;base64," +*/ imageData;
-          console.log(imageData);
-
-          return imageData;
-        }, function (err) {
-          // error
-        });
-      };
 
       $scope.sendPicture = function (URL, image) {
         return image.then(function (imageData) {
             return PictureUploadService.upload(imageData, URL);
           }
         );
+      };
+
+      $scope.readyCamera = function(){
+        PhotoService.readyCamera();
+      };
+
+      $scope.takePicture = function(){
+        return PhotoService.takePicture(PhotoService.cameraDefaultOps);
       };
 
       $scope.close = function () {
