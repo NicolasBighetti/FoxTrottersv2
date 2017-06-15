0b88b24e55a84e2abc32a67c8c634a321e3c4cdd
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-15 12:12:04 +0200

    [correction] PIAM-168 le controlleur de la reconnaissance marche
    Et reconnait

--- foxapp/www/js/controllers/ListResultController.js
+++ foxapp/www/js/controllers/ListResultController.js
@@ -3,9 +3,11 @@
  */
 angular.module('foxapp')
 
-.controller('ListResultController',['$scope','PhotoService',
-  function($scope, PhotoService){
+.controller('ListResultController',['$scope','PhotoService','PictureUploadService','RESTService',
+  function($scope, PhotoService, PictureUploadService, RESTService){
       $scope.listResult = [];
+
+      $scope.showResults = true;
 
       console.log(PhotoService.cameraDefaultOps);
 
@@ -16,28 +18,39 @@ angular.module('foxapp')
         return PhotoService.takePicture(PhotoService.cameraDefaultOps);
       };
 
+      $scope.uploadPhoto = function(){
+
+      };
+
     $scope.recognizePlant = function(promise){
       promise.then(
         function(data){
           console.log("Data : " + data);
-          var json = JSON.parse(data);
-          console.log("been there");
 
-          RESTService.get(dbPath + "api/planders/result/"+json._id).then(
-            function(result){
+          if(data === undefined){
+            return;
+          }
+
+          PictureUploadService.upload(data, dbPath + "api/planders/").then(
+            function(data) {
+              var json = JSON.parse(data);
+              console.log("been there");
+
+              RESTService.get(dbPath + "api/planders/result/" + json._id).then(
+                function (result) {
 
 
-              console.log("Result : " + result);
-              var jsson = JSON.parse(result);
+                  console.log("Result : " + result);
+                  $scope.listResult = JSON.parse(result);
+                  $scope.showResults = false;
+                  console.log($scope.listResult);
 
-              $scope.answerder = jsson.results[0].name;
-
-              $scope.listResult = jsson.splice(0, 5);
-
-              console.log($scope.listResult);
-
+                }
+              );
             }
           );
+
+
         }
       );
     };
