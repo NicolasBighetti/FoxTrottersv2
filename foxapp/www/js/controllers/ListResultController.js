9a52ba6a7909cc8c7ac87d036b98615898fea5be
Nicolas Bighetti <nicobighetti@gmail.com>
2017-06-15 12:15:48 +0200

    [clean] PIAM-168 retrait des console.log

--- foxapp/www/js/controllers/ListResultController.js
+++ foxapp/www/js/controllers/ListResultController.js
@@ -8,13 +8,10 @@ angular.module('foxapp')
       $scope.listResult = [];
 
       $scope.showResults = true;
-
-      console.log(PhotoService.cameraDefaultOps);
-
+    
       PhotoService.readyCamera();
 
       $scope.takePicture = function(){
-        console.log('shaprrr');
         return PhotoService.takePicture(PhotoService.cameraDefaultOps);
       };
 
@@ -25,8 +22,6 @@ angular.module('foxapp')
     $scope.recognizePlant = function(promise){
       promise.then(
         function(data){
-          console.log("Data : " + data);
-
           if(data === undefined){
             return;
           }
@@ -34,17 +29,11 @@ angular.module('foxapp')
           PictureUploadService.upload(data, dbPath + "api/planders/").then(
             function(data) {
               var json = JSON.parse(data);
-              console.log("been there");
 
               RESTService.get(dbPath + "api/planders/result/" + json._id).then(
                 function (result) {
-
-
-                  console.log("Result : " + result);
                   $scope.listResult = JSON.parse(result);
                   $scope.showResults = false;
-                  console.log($scope.listResult);
-
                 }
               );
             }
